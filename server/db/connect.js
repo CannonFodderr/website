0101ca7a33adbe94afbc28c061eeaa42f72afd56
Idan Izhaki <izhaki.idan@gmail.com>
2018-10-23 22:31:57 +0300

    Fix for db undifiened

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,5 +1,4 @@
 const env = require('dotenv').config();
-// const config = require('../../dbconfig');
 const Sequelize = require('sequelize');
 
 dbAuth = (db, dbName) => {
@@ -13,35 +12,36 @@ dbAuth = (db, dbName) => {
 }
 
 let generalDB = () => {
-// RUN DEVELOPMENT
-if (process.env.DB_STATE == 'dev') {
-    let devDB = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        host: process.env.HOST,
-        dialect: 'postgres',
-        operatorsAliases: false,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    });
-    // dbAuth(devDB, process.env.DB_NAME)
-    return devDB
-}
-// RUN PRODUCTION
-if (process.env.DB_STATE == 'prod') {
-    let prodDB = new Sequelize(process.env.PROD_DB_NAME, process.env.PROD_DB_USER, process.env.PROD_DB_PASS, {
-        host: process.env.PROD_DB_HOST,
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl:true
-        }
-
-    });
-    // dbAuth(prodDB, process.env.DB_NAME);
-    return prodDB
-}
+    // RUN DEVELOPMENT
+    if (process.env.DB_STATE == 'dev') {
+        let devDB = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+            host: process.env.HOST,
+            dialect: 'postgres',
+            operatorsAliases: false,
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+            }
+        });
+        dbAuth(devDB, process.env.DB_NAME);
+        return devDB
+    }
+    // RUN PRODUCTION
+    if (process.env.DB_STATE == 'prod') {
+        let prodDB = new Sequelize(process.env.PROD_DB_NAME, process.env.PROD_DB_USER, process.env.PROD_DB_PASS, {
+            host: process.env.PROD_DB_HOST,
+            dialect: 'postgres',
+            logging: false,
+            dialectOptions: {
+                ssl:true
+            }
+            
+        });
+        dbAuth(prodDB, process.env.DB_NAME);
+        return prodDB
+    }
 }
 let db = generalDB()
 
