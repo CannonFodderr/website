fb946583aeecd7886849631d7e48787613ba0f45
Idan Izhaki <izhaki.idan@gmail.com>
2018-10-31 14:55:49 +0200

    Add Kruvi to the thank you template

--- server/mailer/templates.js
+++ server/mailer/templates.js
@@ -1,7 +1,7 @@
 const templates = {
     thankYou: {
         subject: "Thank You",
-        body: "<h2>Thank you for contacting me</h2><p>I'll get back to you asap</p><p>Best Regrads,</p><p>Idan Izhaki</p>"
+        body: "<img style=`height:200px;width:auto` src=`https://image.ibb.co/fCY6JL/kruv.jpg` alt=`kruv` border=`0`><h2>Thank you for contacting me</h2><p>I'll get back to you ASAP!</p><p>Best Regrads,</p><p>Idan Izhaki</p>"
     }
 }
 
