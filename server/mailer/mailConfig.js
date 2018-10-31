4a2642f051b4b28d854dde6490b12b9f740650b2
Idan Izhaki <izhaki.idan@gmail.com>
2018-10-31 23:42:33 +0200

    Moved nodemailer auth to oAuth2

--- server/mailer/mailConfig.js
+++ server/mailer/mailConfig.js
@@ -1,13 +1,21 @@
 const env = require('dotenv').config()
-
+const { google } = require('googleapis');
 const nodemailer = require('nodemailer');
-// let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.EMAIL_PASS
-//     }
-// })
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+    process.env.OAUTH_ID,
+    process.env.OAUTH_SECRET,
+    "https://developers.google.com/oauthplayground"
+)
+
+oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+});
+
+const accessToken = oauth2Client.getAccessToken()
+.then(res => { res.access_token })
+.catch(e => { console.error(e)})
 
 let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -18,19 +26,11 @@ let transporter = nodemailer.createTransport({
         user: process.env.EMAIL,
         clientId: process.env.GOOGLE_PLUS_ID,
         clientSecret: process.env.OAUTH_SECRET,
-        accessToken: process.env.ACCESS_TOKEN,
         refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
     }
 });
 
-transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
-    let accessToken = userTokens[user];
-    if(!accessToken){
-        return callback(new Error('Unknown user'));
-    }else{
-        return callback(null, accessToken);
-    }
-});
 
 transporter.on('token', token => {
     console.log('A new access token was generated');
