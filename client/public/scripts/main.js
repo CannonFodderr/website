159a1a1d6bfbeefbf65fa1fae869558470c8f4a7
Idan Izhaki <izhaki.idan@gmail.com>
2018-11-01 16:58:00 +0200

    Add serviceworker ignore routes to ADMIN | PROJECTS

--- client/public/scripts/main.js
+++ client/public/scripts/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator){
     navigator.serviceWorker.register('/sw.js')
     .then((reg)=>{
-        console.log('Registered Serviceworker');
+        
     })
     .catch(e => {
         console.error('Registration failed with ' + e)
