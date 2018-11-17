47e5f9d22d3b8164379bf7ba98ad7aab3b4e4258
Idan Izhaki <izhaki.idan@gmail.com>
2018-11-17 18:55:54 +0200

    Rework for index login

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -14,7 +14,7 @@ router.get('/profile/:userId/projects', (req, res)=>{
         {user_id: req.params.userId, category: req.query.category }
         ,order: [['order', 'asc']], include: [Icon, User]})
         .then((foundProjects)=>{
-            return res.render('./projects/all', { projects: foundProjects, title: `${req.query.category} projects`, user: foundProjects[0].User })
+            return res.render('./projects/all', { projects: foundProjects, title: `${req.query.category} projects`, user: foundProjects[0].User, csrf: req.csrfToken() })
         })
         .catch(e => {
             console.error(e);
@@ -22,7 +22,7 @@ router.get('/profile/:userId/projects', (req, res)=>{
         })
     } else {
         Project.findAll({where: { user_id: req.params.userId},order: [['order', 'asc']], include: [User, Icon]}).then((allProjects)=>{
-            return res.render('./projects/all', { projects: allProjects, title: `All projects`, user: allProjects[0].User })
+            return res.render('./projects/all', { projects: allProjects, title: `All projects`, user: allProjects[0].User, csrf: req.csrfToken() })
         }).catch(e => {
             console.error(e);
             res.redirect('back');
